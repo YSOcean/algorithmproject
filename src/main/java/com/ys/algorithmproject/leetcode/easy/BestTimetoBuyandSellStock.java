c2aa2cd6f6ad1e0ee836b25adb216443208100ff
YSOcean <18271265732@163.com>
2019-04-01 22:59:35 +0800

    提交 121. Best Time to Buy and Sell Stock

---
+++ src/main/java/com/ys/algorithmproject/leetcode/easy/BestTimetoBuyandSellStock.java
@@ -0,0 +1,29 @@
+package com.ys.algorithmproject.leetcode.easy;
+
+import javax.validation.constraints.Max;
+
+/**
+ * Create by YSOcean
+ * 题目：假设有一个数组，其中第i个元素是当天股票的价格，一次只能完成一比交易（买一份股票，然后卖出去）
+ *      根据给定的股票价格，算出买卖的最大利润
+ * 注意：没买之前是不能出售股票的
+ * 例子：[7,1,5,3,6,4]，我们算出最大利润为5（在第二天买入股票，价格为1，然后在第5天卖掉，价格为6，差价为6-1=5）
+ *       [7,6,4,3,1]，我们算出最大利润为0，因为这是一个越来越小的数组，卖出的价格一定小于等于买入的价格。
+ */
+public class BestTimetoBuyandSellStock {
+
+    /**
+     * 只需要遍历一次数组，用一个变量记录遍历过数中的最小值，
+     * 然后每次计算当前值和这个最小值之间的差值最为利润，
+     * 然后每次选较大的利润来更新。当遍历完成后当前利润即为所求
+     */
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int price : prices) {
+            min = Math.min(min,price);
+            max = Math.max(price-min,max);
+        }
+        return max;
+    }
+}
