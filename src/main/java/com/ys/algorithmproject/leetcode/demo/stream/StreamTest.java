e0d017c1fff6ac39df78be1513ae5f0495123d61
YSOcean <18271265732@163.com>
2019-04-13 23:14:57 +0800

    Lambda Stream

--- src/main/java/com/ys/algorithmproject/leetcode/demo/stream/StreamTest.java
+++ src/main/java/com/ys/algorithmproject/leetcode/demo/stream/StreamTest.java
@@ -1,7 +1,10 @@
 package com.ys.algorithmproject.leetcode.demo.stream;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Create by YSOcean
@@ -20,7 +23,25 @@ public class StreamTest {
                 new Dash("prawns",false,300,Dash.Type.FISH),
                 new Dash("salmon",false,450,Dash.Type.FISH)
         );
+        List<Dash> isVegetarian =
+                menu.stream().filter( a -> a.isVegetarian()).collect(Collectors.toList());
+        List<Dash> isVegetarian2 =
+                menu.stream().filter(Dash::isVegetarian).collect(Collectors.toList());
 
 
+        //筛选各异的元素distinct
+        List<Integer> nums = Arrays.asList(1,2,1,3,3,2,4);
+        nums.stream().filter(i -> i%2==0).distinct()
+                .forEach(System.out::println);
+
+        //截断流limit
+        List<Dash> dishes = menu.stream().filter(d -> d.getCalories()>300)
+                .limit(3).collect(Collectors.toList());
+
+        //跳过元素
+        List<Dash> dishSkip = menu.stream().filter(d -> d.getCalories()>300)
+                .skip(2).collect(Collectors.toList());
+
+        System.out.println("**********");
     }
 }
