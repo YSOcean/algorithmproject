0cd7d5f99652e6295d9e356297b90c2d8bcca9c9
YSOcean <18271265732@163.com>
2019-06-17 22:07:52 +0800

    插入排序-选择排序

--- src/main/java/com/ys/algorithmproject/leetcode/demo/sort/SortTest.java
+++ src/main/java/com/ys/algorithmproject/leetcode/demo/sort/SortTest.java
@@ -7,6 +7,11 @@ import java.util.Arrays;
  */
 public class SortTest {
 
+    /**
+     * 冒泡排序
+     * @param array
+     * @return
+     */
     public static int[] bubbleSort(int[] array){
         boolean flag = true;
         for (int i = 1; i < array.length; i++) {
@@ -25,9 +30,54 @@ public class SortTest {
         return array;
     }
 
+    /**
+     * 选择排序（不稳定）
+     * @param array
+     * @return
+     */
+    public static int[] choiceSort(int[] array){
+
+        for (int i = 0; i < array.length-1; i++) {
+            int min = i;
+            for (int j = i+1; j < array.length; j++) {
+                if(array[j]<array[min]){
+                    min = j;
+                }
+            }
+            if(min != i){
+                int temp = array[min];
+                array[min] = array[i];
+                array[i] = temp;
+            }
+        }
+        return array;
+    }
+
+    /**
+     * 插入排序
+     * @param array
+     * @return
+     */
+    public static int[] insertSort(int[] array){
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i;
+            while(j>0 && temp<array[j-1]){
+                array[j] = array[j-1];
+                j--;
+            }
+            array[j] = temp;
+        }
+        return array;
+    }
+
     public static void main(String[] args) {
         int[] array = {3,5,1,9,8,2,7,6,4};
-        bubbleSort(array);
+        //bubbleSort(array);
+
+        //choiceSort(array);
+
+        insertSort(array);
         System.out.println(Arrays.toString(array));
     }
 }
