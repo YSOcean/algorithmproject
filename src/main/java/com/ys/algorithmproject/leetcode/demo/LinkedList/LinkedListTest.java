b3443062533c5e6b322804123978f0333c4e3eb1
YSOcean <18271265732@163.com>
2019-06-19 23:13:49 +0800

    反转链表

---
+++ src/main/java/com/ys/algorithmproject/leetcode/demo/LinkedList/LinkedListTest.java
@@ -0,0 +1,96 @@
+package com.ys.algorithmproject.leetcode.demo.LinkedList;
+
+
+/**
+ * Create by YSOcean
+ */
+public class LinkedListTest {
+
+    public static void main(String[] args) {
+        LinkedNode l1 = new LinkedNode("A");
+        LinkedNode l2 = new LinkedNode("B");
+        LinkedNode l3 = new LinkedNode("C");
+        LinkedNode l4 = new LinkedNode("D");
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+
+        print(l1);
+        print(reverseList2(l1));
+    }
+
+    /**
+     * 递归反转链表
+     * @param head
+     * @return
+     */
+    public static LinkedNode reverseList1(LinkedNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        LinkedNode newList = reverseList1(head.next);
+        LinkedNode l2 = head.next;
+        l2.next = head;
+        head.next = null;
+        return newList;
+    }
+
+    /**
+     * 反转链表2
+     * @param head
+     * @return
+     */
+    public static LinkedNode reverseList2(LinkedNode head){
+        LinkedNode pre = null;
+        LinkedNode current = null;
+        while(head != null){
+            current = head.next;
+            head.next = pre;
+            pre = head;
+            head = current;
+        }
+        return pre;
+
+    }
+
+
+    /**
+     * 反转链表3
+     * @param head
+     * @return
+     */
+    public static LinkedNode reverseList3(LinkedNode head){
+        LinkedNode pre = null;
+        LinkedNode current = head;
+        LinkedNode next = null;
+
+        while(head != null){
+            current = head.next;
+            head.next = pre;
+            pre = head;
+            head = current;
+        }
+        return pre;
+
+    }
+
+    /**
+     * 打印链表
+     * @param head
+     */
+    public static void print(LinkedNode head){
+        if(head == null){
+            System.out.println("空链表！！！");
+        }
+        LinkedNode current = head;
+        while(current != null){
+            if(current.next == null){
+                System.out.print(current.val+"->"+current.next);
+                System.out.println();
+            }else{
+                System.out.print(current.val+"->");
+            }
+            current = current.next;
+        }
+    }
+}
---
+++ src/main/java/com/ys/algorithmproject/leetcode/demo/LinkedList/LinkedNode.java
@@ -0,0 +1,14 @@
+package com.ys.algorithmproject.leetcode.demo.LinkedList;
+
+/**
+ * Create by YSOcean
+ */
+public class LinkedNode {
+    public String val;
+    public LinkedNode next;
+
+    public LinkedNode(String val){
+        this.val = val;
+    }
+
+}
