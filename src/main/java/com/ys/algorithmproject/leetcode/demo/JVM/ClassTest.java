6810ee2db9d264b3c467761579b9e356f3d81db0
YSOcean <18271265732@163.com>
2019-12-15 22:42:18 +0800

    增加application-prod.yml

---
+++ src/main/java/com/ys/algorithmproject/leetcode/demo/JVM/ClassTest.java
@@ -0,0 +1,13 @@
+package com.ys.algorithmproject.leetcode.demo.JVM;
+
+/**
+ * Create by YSOcean
+ */
+public class ClassTest {
+    private static int i = 0;
+
+    public static void main(String[] args) {
+        System.out.println(i);
+
+    }
+}
