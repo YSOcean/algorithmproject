11f70d165d47527169f593d57efe333faaf0a76c
yushuai <yushuai@shimo.im>
2022-05-31 11:19:35 +0800

    add 判断是否是二叉搜索树

---
+++ src/main/java/com/ys/algorithmproject/leetcode/tree/IsValidBST.java
@@ -0,0 +1,20 @@
+package com.ys.algorithmproject.leetcode.tree;
+
+//https://leetcode.cn/problems/validate-binary-search-tree/
+// 判断是否是二叉搜索树
+public class IsValidBST {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+}
